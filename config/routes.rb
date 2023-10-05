be2236d5846bb7c879e44eed935939d47d30b4ee
Adrian Ramirez <adrian.ramirez45679@gmail.com>
2023-10-05 10:33:17 -0700

    almost finishded good luckgit status

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get '/blog_pages' => 'blog_page#index', as: 'blog_pages'
+  post '/blog_pages' => 'blog_page#create', as: 'create_blog_page'
+  get '/blog_pages/new' => 'blog_page#new', as: 'new_blog_page'
+  get '/blog_pages/:id/edit' => 'blog_page#edit', as: 'edit_blog_page'
+  patch '/blog_pages/:id' => 'blog_page#update'
+  get '/blog_pages/:id' => 'blog_page#show', as: 'blog_page'
 end
